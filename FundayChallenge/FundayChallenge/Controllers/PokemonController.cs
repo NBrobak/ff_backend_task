63e7803ea83207524ea704a3321b93f65e178f2f
Unknown <nicolai-brobak@hotmail.com>
2018-12-04 12:46:41 +0100

    Recreated project with proper docker for Linux and pomelo MySQL

---
+++ FundayChallenge/FundayChallenge/Controllers/PokemonController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FundayChallenge.Models;
+
+namespace FundayChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    public class PokemonController : Controller
+    {
+		private readonly PokemonContext _context;
+
+		public PokemonController(PokemonContext context)
+		{
+			_context = context;
+		}
+
+        [HttpGet]
+        public APIResult Get()
+        {
+			//Pokemon[] pokemons = new Pokemon[] {
+			//    new Pokemon{ Id = 0, Name = "Growlithe", Evolution = 0, Types = new List<string>{ "Fire" }, IsLegendary = false},
+			//    new Pokemon{ Id = 0, Name = "Arcanine", Evolution = 1, Types = new List<string>{ "Fire" }, IsLegendary = false} };
+
+			IEnumerable<Pokemon> pokemons = _context.Pokemons;
+            APIResult result = new APIMultiResult(pokemons);
+
+            return result;
+        }
+        
+        [HttpGet("{id}")]
+        public APIResult Get(int id)
+        {
+			//Pokemon pokemon = new Pokemon { Id = 0, Name = "Growlithe", Evolution = 0, Types = new List<string> { "Fire" }, IsLegendary = false };
+
+			Pokemon pokemon = _context.Find<Pokemon>(id);
+
+			if (pokemon == null)
+			{
+				return new APISingleResult("404: Pokemon not found.");
+			}
+
+			APIResult result = new APISingleResult(pokemon);
+
+            return result;
+        }
+        
+        [HttpPost]
+        public int Post([FromBody]Pokemon value)
+        {
+
+
+            return 0;
+        }
+    }
+}
---
+++ FundayChallenge/FundayChallenge/Models/PokemonContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FundayChallenge.Models
+{
+	public class PokemonContext : DbContext
+	{
+		public DbSet<Pokemon> Pokemons { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Pokemon>().HasKey("Id");
+		}
+	}
+}
